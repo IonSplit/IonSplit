3221c4db8f6132018aac3b25565d47bf607045b7
Ahmed Nefzaoui <nefzaoui.ahmed@hotmail.com>
2017-10-15 19:07:36 +0100

    Foundation for save file parsing APIs

---
+++ addons/runner/save_parser.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+function SAVE_PARSER() {}
+
+SAVE_PARSER.prototype.init = function() {
+
+};
+
+SAVE_PARSER.prototype.getMissions = function() {
+  // FIXME:
+  return [];
+};
+
+SAVE_PARSER.prototype.getMissionTimes = function(name) {
+  // FIXME:
+};
+
+SAVE_PARSER.prototype.getMissionsGeneralInfo = function(name) {
+  // FIXME:
+};
+
+module.exports = SAVE_PARSER;
